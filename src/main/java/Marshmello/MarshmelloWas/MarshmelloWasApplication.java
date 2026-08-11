aa1139b2d1cc0d5467ae1ae3882c2a686f9b4fad
PSJ <seongjun1228@gmail.com>
2026-08-12 05:33:43 +0900

    feat: 로컬 초기 파일 추가

---
+++ src/main/java/Marshmello/MarshmelloWas/MarshmelloWasApplication.java
@@ -0,0 +1,13 @@
+package Marshmello.MarshmelloWas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarshmelloWasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarshmelloWasApplication.class, args);
+	}
+
+}
