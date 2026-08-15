ea0f303f336aa1d2d748ee4bf71d94beb5b3055f
PSJ <seongjun1228@gmail.com>
2026-08-16 03:09:00 +0900

    test: 포트:5173 활성화

--- src/main/java/Marshmello/MarshmelloWas/infrastructure/auth/config/SecurityConfig.java
+++ src/main/java/Marshmello/MarshmelloWas/infrastructure/auth/config/SecurityConfig.java
@@ -73,7 +73,7 @@ public class SecurityConfig {
                     .authorizationRequestRepository(authorizationRequestRepository)
                     .authorizationRequestResolver(authorizationRequestResolver)
                 )
-                .defaultSuccessUrl("/", true)
+                .defaultSuccessUrl("http://localhost:5173", true)
             )
             .oauth2Client(client -> client
                 .authorizedClientRepository(authorizedClientRepository)
